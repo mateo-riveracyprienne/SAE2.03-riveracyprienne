6631d220a5c39d63e9a17828a7478d0eb570d834
Rivera--Cyprienne Mateo <riveracyprien1@mmi-dev.unilim.fr>
2026-04-23 14:09:07 +0200

    Le poulet pané c'est bon

--- app/component/Movies/script.js
+++ app/component/Movies/script.js
@@ -1,13 +1,17 @@
 let templateFile = await fetch("./component/Movies/template.html");
 let template = await templateFile.text();
 let templateLiFile = await fetch("./component/Movies/templateLi.html");
-let templateLi = await templateFile.text();
+let templateLi = await templateLiFile.text();
 
 let Movies = {};
 
-Movies.format = function (DataMovie) {
+Movies.format = function (DataMovie, tab) {
   let html = template;
   let movieLi = "";
+
+  if (DataMovie == null){
+    html.replace("{{movieList}}", "<p class='movies__error'>Aucun film disponible pour le moment.</p>");
+  }
     for (let movie of DataMovie) {
         let li = templateLi;
         li = li.replace("{{title}}", movie.name);
