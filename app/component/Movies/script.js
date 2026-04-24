1b064629211ca600070370bec1ed4b2fe57a82f3
Rivera--Cyprienne Mateo <riveracyprien1@mmi-dev.unilim.fr>
2026-04-24 10:22:13 +0200

    Itération 1

--- app/component/Movies/script.js
+++ app/component/Movies/script.js
@@ -5,21 +5,26 @@ let templateLi = await templateLiFile.text();
 
 let Movies = {};
 
-Movies.format = function (DataMovie, tab) {
+Movies.format = function (DataMovie) {
   let html = template;
   let movieLi = "";
 
   if (DataMovie == null){
-    html.replace("{{movieList}}", "<p class='movies__error'>Aucun film disponible pour le moment.</p>");
+    html.replace("{{movieList}}", "<h1>Pas de film disponible</h1>");
   }
+
+  else {
     for (let movie of DataMovie) {
         let li = templateLi;
         li = li.replace("{{title}}", movie.name);
         li = li.replace("{{image}}", movie.image);
         movieLi += li;
     }
-  html = html.replace("{{movieList}}", movieLi);
+    html = html.replace("{{movieList}}", movieLi);
+  }
+
   return html;
+  
 };
 
 export { Movies };
